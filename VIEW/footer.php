2876c25df2f97a392525ad80ec030307720ec425
BertrandVALENTIN <bertrandvalentin339@gmail.com>
2017-04-28 18:22:03 +0100

    admins

--- VIEW/footer.php
+++ VIEW/footer.php
@@ -1,6 +1,7 @@
 
 	<footer>
-		Vente de livres - 2017 - Contact: <a href="mailto:admin&#64;mailinator.com" title="MFsupport">Bertrand VALENTIN</a>
+		Vente de livres - 2017 - Contact: <a href="mailto:admin&#64;mailinator.com" title="MFsupport">BENAICHE Ali</a> -
+		<a href="mailto:admin&#64;mailinator.com" title="MFsupport">Bertrand VALENTIN</a>
 	</footer>
 </body>
 </html>
